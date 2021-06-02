3924229535c803bda3740216206a905927a48211
FlutLab.io <>
2021-06-02 22:02:09 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedloginwidget/GeneratedLoginWidget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedinputwidget2/GeneratedInputWidget2.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedeyewidget/GeneratedEyeWidget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedbotonwidget1/GeneratedBotonWidget1.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedmenuinferiorwidget/GeneratedMenuinferiorWidget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatediniciowidget1/GeneratedInicioWidget1.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedpedidoswidget4/GeneratedPedidosWidget4.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedpedidoswidget7/GeneratedPedidosWidget7.dart';
+import 'package:flutterapp/lirucisa_iosapp/generateddetalledelaordenwidget/GeneratedDetalledelaOrdenWidget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedperfilwidget10/GeneratedPerfilWidget10.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedportraitwidget1/GeneratedPortraitWidget1.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedajusteswidget12/GeneratedAjustesWidget12.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedmenusuperiorwidget5/GeneratedMenusuperiorWidget5.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedbuscadorwidget1/GeneratedBuscadorWidget1.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedpizzaswidget1/GeneratedPizzasWidget1.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedimagendeordenwidget1/GeneratedImagendeordenWidget1.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedframe2widget/GeneratedFrame2Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generateddetalleswidget1/GeneratedDetallesWidget1.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedgroup34widget/GeneratedGroup34Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedgroup33widget/GeneratedGroup33Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedgroup35widget/GeneratedGroup35Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedgroup32widget/GeneratedGroup32Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedgroup36widget/GeneratedGroup36Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedgroup31widget/GeneratedGroup31Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedgroup37widget/GeneratedGroup37Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedgroup30widget/GeneratedGroup30Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedgroup38widget/GeneratedGroup38Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedframe4widget/GeneratedFrame4Widget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedhomesharpwidget/GeneratedHomesharpWidget.dart';
+import 'package:flutterapp/lirucisa_iosapp/generatedbriefcasesharpwidget/GeneratedBriefcasesharpWidget.dart';
+
+void main() {
+  runApp(Lirucisa_IOSApp());
+}
+
+class Lirucisa_IOSApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedLoginWidget',
+      routes: {
+        '/GeneratedLoginWidget': (context) => GeneratedLoginWidget(),
+        '/GeneratedInputWidget2': (context) => GeneratedInputWidget2(),
+        '/GeneratedEyeWidget': (context) => GeneratedEyeWidget(),
+        '/GeneratedBotonWidget1': (context) => GeneratedBotonWidget1(),
+        '/GeneratedMenuinferiorWidget': (context) =>
+            GeneratedMenuinferiorWidget(),
+        '/GeneratedInicioWidget1': (context) => GeneratedInicioWidget1(),
+        '/GeneratedPedidosWidget4': (context) => GeneratedPedidosWidget4(),
+        '/GeneratedPedidosWidget7': (context) => GeneratedPedidosWidget7(),
+        '/GeneratedDetalledelaOrdenWidget': (context) =>
+            GeneratedDetalledelaOrdenWidget(),
+        '/GeneratedPerfilWidget10': (context) => GeneratedPerfilWidget10(),
+        '/GeneratedPortraitWidget1': (context) => GeneratedPortraitWidget1(),
+        '/GeneratedAjustesWidget12': (context) => GeneratedAjustesWidget12(),
+        '/GeneratedMenusuperiorWidget5': (context) =>
+            GeneratedMenusuperiorWidget5(),
+        '/GeneratedBuscadorWidget1': (context) => GeneratedBuscadorWidget1(),
+        '/GeneratedPizzasWidget1': (context) => GeneratedPizzasWidget1(),
+        '/GeneratedImagendeordenWidget1': (context) =>
+            GeneratedImagendeordenWidget1(),
+        '/GeneratedFrame2Widget': (context) => GeneratedFrame2Widget(),
+        '/GeneratedDetallesWidget1': (context) => GeneratedDetallesWidget1(),
+        '/GeneratedGroup34Widget': (context) => GeneratedGroup34Widget(),
+        '/GeneratedGroup33Widget': (context) => GeneratedGroup33Widget(),
+        '/GeneratedGroup35Widget': (context) => GeneratedGroup35Widget(),
+        '/GeneratedGroup32Widget': (context) => GeneratedGroup32Widget(),
+        '/GeneratedGroup36Widget': (context) => GeneratedGroup36Widget(),
+        '/GeneratedGroup31Widget': (context) => GeneratedGroup31Widget(),
+        '/GeneratedGroup37Widget': (context) => GeneratedGroup37Widget(),
+        '/GeneratedGroup30Widget': (context) => GeneratedGroup30Widget(),
+        '/GeneratedGroup38Widget': (context) => GeneratedGroup38Widget(),
+        '/GeneratedFrame4Widget': (context) => GeneratedFrame4Widget(),
+        '/GeneratedHomesharpWidget': (context) => GeneratedHomesharpWidget(),
+        '/GeneratedBriefcasesharpWidget': (context) =>
+            GeneratedBriefcasesharpWidget(),
+      },
+    );
+  }
+}
